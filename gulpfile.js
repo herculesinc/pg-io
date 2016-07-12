98c477e231202b09da07742b017ffc99adb4900f
Irakliy <irakliy81@yahoo.com>
2016-07-12 11:27:41 -0700

    updated build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,20 @@
-var gulp = require('gulp');
-var del = require('del');
-var exec = require('child_process').exec;
-var mocha = require('gulp-mocha');
+'use strict';
+// IMPORTS
+// ================================================================================================
+const gulp  = require('gulp');
+const del   = require('del');
+const exec  = require('child_process').exec;
+const mocha = require('gulp-mocha');
+const gutil = require('gulp-util');
 
+// TASKS
+// ================================================================================================
 // delete bin folder
 gulp.task('clean', function(cb) {
-  del(['bin'], cb);
+  del(['bin']).then(() => { cb(); });
 });
 
 // compile TypeScript files
-gulp.task('build', ['compile'], function (cb) {
-  gulp.src('./package.json').pipe(gulp.dest('./bin'));
-  gulp.src('./.settings/.npmignore').pipe(gulp.dest('./bin'));
-  gulp.src('./README.md').pipe(gulp.dest('./bin'));
-  cb();
-});
-
-// compile task (TypeScript->ES6)
 gulp.task('compile', ['clean'], function (cb) {  
   exec('tsc -p .', function (err, stdout, stderr) {
     if (stdout.length > 0) console.log(stdout);
@@ -25,13 +23,25 @@ gulp.task('compile', ['clean'], function (cb) {
   });
 });
 
+// build the project
+gulp.task('build', ['compile'], function (cb) {
+  gulp.src('./package.json').pipe(gulp.dest('./bin'));
+  gulp.src('./.settings/.npmignore').pipe(gulp.dest('./bin'));
+  gulp.src('./README.md').pipe(gulp.dest('./bin'));
+  cb();
+});
+
 // run tests
 gulp.task('test', ['build'], function () {
-    return gulp.src('./bin/tests/**/*.js', {read: false})
-        .pipe(mocha({reporter: 'spec'}))
-        .once('end', function () {
-          process.exit();
-        });
+    return gulp.src('./bin/tests/**/*.js', { read: false })
+        .pipe( mocha( { reporter: 'spec', timeout: 25000, bail: false } ) )
+        .on( 'error', err => {
+            if ( err && ( !err.message || !err.message.match( /failed/ ) ) ) {
+                gutil.log( gutil.colors.red( JSON.stringify( err, null, 2 ) ) );
+            }
+        } )
+        .once( 'error', () => process.exit( 1 ) )
+        .on( 'end', () =>  process.exit( 0 ) );
 });
 
 // publish to npm
